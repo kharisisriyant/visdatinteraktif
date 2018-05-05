94fc4e29e35d603774d370ad7923f1a4eb616a47
ParadiseCatz <christiananthonysetyawan@gmail.com>
2018-05-05 09:34:35 +0700

    Add sly framework

--- js/script.js
+++ js/script.js
@@ -14,45 +14,50 @@ d3.selection.prototype.moveToFront = function() {
 d3.selection.prototype.moveToBack = function() { return this.each(function() { var firstChild = this.parentNode.firstChild; if (firstChild) { this.parentNode.insertBefore(this, firstChild); } }); };
 
 // Create SVG element
-var svg = d3.select("body").insert("svg", "p")
+var svg = d3.select("#idnMap").insert("svg", "p")
             .attr("width", width)
-            .attr("height", height * 0.8)
+            .attr("height", height * 0.5)
             .attr("class", "map");
+var g = svg.append("g");
+// Color
+var populationColor = d3.scaleThreshold()
+                        .domain(populationDomain)
+                        .range(colorRange);
+
 
 // Projection and path
 var projection = d3.geoMercator()
                     .center([118.25, -5])
                     .scale(width * 0.5)
-                    .translate([width / 3, height / 3]);
+                    .translate([width / 3, height / 4]);
 
 var path = d3.geoPath().projection(projection);
 
 // Asynchronous tasks, load topojson map and data
 d3.queue()
   .defer(d3.json, "data/IDN.json")
-  .defer(d3.csv, "data/IDN.csv")
+  .defer(d3.json, "data/DatabaseSBMPTN2017.json")
   .await(ready);
-
+var sbmptnData;
 // Callback function
-function ready(error, data, population) {
+function ready(error, idnSpatialData, sbmptnDataResult) {
   if (error) throw error;
 
   // Population data
   var populationData = {};
-
-  population.forEach(function(d) { populationData[d.id] = +d.population; });
-
-  // Color
-  var populationColor = d3.scaleThreshold()
-                          .domain(populationDomain)
-                          .range(colorRange);
-
-  var g = svg.append("g");
+  console.log("ASDASD")
+  sbmptnData = sbmptnDataResult;
+  sbmptnData.forEach(function(d) { 
+    if (populationData[d.Provinsi] == null) {
+      populationData[d.Provinsi] = 0
+    }
+    populationData[d.Provinsi] += parseInt(d.Jumlah);
+  });
 
   // Draw the map
   g.selectAll("path")
     .attr("class", "city")
-    .data(topojson.feature(data, data.objects.IDN).features)
+    .data(topojson.feature(idnSpatialData, idnSpatialData.objects.IDN).features)
     .enter()
     .append("path")
     .attr("d", path)
@@ -80,7 +85,7 @@ function ready(error, data, population) {
   g.selectAll("path")
     .append("title")
     .text(function(d) {
-      return d.properties.NAME_1 + " : " + populationData[d.properties.ID_2];
+      return d.properties.NAME_1 + " : " + populationData[d.properties.NAME_1];
     });
 }
 
@@ -91,12 +96,43 @@ function resize() {
   height = window.innerHeight;
 
   projection.scale(width * 0.5)
-            .translate([width / 3, height / 3]);
+            .translate([width / 3, height / 4]);
 
   d3.select("svg")
     .attr("width", width)
-    .attr("height", height * 0.8);
+    .attr("height", height * 0.5);
 
   d3.selectAll("path")
     .attr("d", path);
 }
+var slyelement = {
+  obj: {},
+  el: '.frame',
+  options: {
+    horizontal: 1,
+    itemNav: 'forceCentered',
+    smart: 1,
+    activateMiddle: 1,
+    activateOn: 'click',
+    mouseDragging: 1,
+    touchDragging: 1,
+    releaseSwing: 1,
+    startAt: 0,
+    scrollBy: 1,
+    speed: 300,
+    elasticBounds: 1,
+    easing: 'swing', // easeInOutElastic, easeOutBounce
+    scrollBar: $('.scrollbar')
+  }
+};
+
+
+$(function(){
+  slyelement.obj = new Sly($(slyelement.el), slyelement.options);
+  
+  slyelement.obj.init();
+});
+
+$(window).resize(function(e) {
+  slyelement.obj.reload();
+});

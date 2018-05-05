0df94f0007a37ccc7af77a1b808ab10764c04ad9
ParadiseCatz <christiananthonysetyawan@gmail.com>
2018-05-05 20:59:37 +0700

    Revert script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
+Array.prototype.removeIf = function(callback) {
+    var i = this.length;
+    while (i--) {
+        if (callback(this[i], i)) {
+            this.splice(i, 1);
+        }
+    }
+};
+
 var width = window.innerWidth,
     height = window.innerHeight,
     populationDomain;
 
-var colorRange = ["#1a9850", "#66bd63", "#a6d96a", "#d9ef8b", "#ffffbf", "#fee08b", "#fdae61", "#f46d43", "#d73027"];
-var populationDomain = [0, 100000, 200000, 300000, 500000, 750000, 1000000, 1500000, 2500000, 5000000];  
+var colorRange = ['#eee','#43a2ca'];
+var populationDomain = [0, 1500, 3000];  
 
 d3.selection.prototype.moveToFront = function() {
   return this.each(function(){
@@ -14,16 +23,26 @@ d3.selection.prototype.moveToFront = function() {
 d3.selection.prototype.moveToBack = function() { return this.each(function() { var firstChild = this.parentNode.firstChild; if (firstChild) { this.parentNode.insertBefore(this, firstChild); } }); };
 
 // Create SVG element
-var svg = d3.select("body").insert("svg", "p")
+var svg = d3.select("#idnMap").insert("svg", "p")
             .attr("width", width)
-            .attr("height", height * 0.8)
+            .attr("height", height * 0.5)
             .attr("class", "map");
+var g = svg.append("g");
+var d3legend;
+var legend = svg.append("g")
+  .attr("class", "legendQuant")
+  .attr("transform", "translate(20,"+(height / 4 + 50)+")");
+// Color
+var populationColorScale = d3.scaleLinear()
+                        .domain(populationDomain)
+                        .range(colorRange);
+
 
 // Projection and path
 var projection = d3.geoMercator()
                     .center([118.25, -5])
                     .scale(width * 0.5)
-                    .translate([width / 3, height / 3]);
+                    .translate([width / 3 - 20, height / 4]);
 
 var path = d3.geoPath().projection(projection);
 
@@ -84,22 +103,30 @@ function ready(error, idnSpatialData, sbmptnDataResult, kodeProdiResult, logoURL
     logoURL[d.NamaUniv] = d.ImgURL;
   })
 
+  kodeProdi = new KodeProdi(kodeProdiResult);
+
   // Population data
   var populationData = {};
-
-  population.forEach(function(d) { populationData[d.id] = +d.population; });
-
-  // Color
-  var populationColor = d3.scaleThreshold()
-                          .domain(populationDomain)
-                          .range(colorRange);
-
-  var g = svg.append("g");
+  console.log("ASDASD")
+  sbmptnData = sbmptnDataResult;
+  sbmptnData.removeIf(function(d) {
+    return kodeProdi.dataByKodeProdi[d.Prodi] == null;
+  })
+  var maxPop = 0;
+  sbmptnData.forEach(function(d) { 
+    if (populationData[d.Provinsi] == null) {
+      populationData[d.Provinsi] = 0
+    }
+    populationData[d.Provinsi] += parseInt(d.Jumlah);
+    maxPop = maxPop < populationData[d.Provinsi] ? populationData[d.Provinsi] : maxPop;
+  });
+  populationColorScale = populationColorScale.domain([0, 1 * maxPop/2, maxPop])
+  createLegend();
 
   // Draw the map
   g.selectAll("path")
     .attr("class", "city")
-    .data(topojson.feature(data, data.objects.IDN).features)
+    .data(topojson.feature(idnSpatialData, idnSpatialData.objects.IDN).features)
     .enter()
     .append("path")
     .attr("d", path)
@@ -110,7 +137,10 @@ function ready(error, idnSpatialData, sbmptnDataResult, kodeProdiResult, logoURL
     .delay(function(d, i) { return Math.random() * 250  +100; })
     .ease(d3.easePolyInOut, 4)
     .attr("fill", function(d) {
-      return populationColor(Math.random() * 5000000);
+      if (populationData[d.properties.province] == null) {
+        populationData[d.properties.province] = 0;
+      }
+      return populationColorScale(populationData[d.properties.province]);
     })
     .attr("transform", "translate(-100,0)scale(1.2)")
     .attr("stroke-width", "0.2")
@@ -127,8 +157,13 @@ function ready(error, idnSpatialData, sbmptnDataResult, kodeProdiResult, logoURL
   g.selectAll("path")
     .append("title")
     .text(function(d) {
-      return d.properties.NAME_1 + " : " + populationData[d.properties.ID_2];
+      return d.properties.province + " : " + populationData[d.properties.province];
     });
+
+  initSly();
+  registerSemuaUnivButton();
+  registerSearchBar();
+  registerSelectProdiDropdown();
 }
 
 d3.select(window).on("resize", resize);
@@ -138,15 +173,15 @@ function resize() {
   height = window.innerHeight;
 
   projection.scale(width * 0.5)
-            .translate([width / 3, height / 3]);
+            .translate([width / 3, height / 4]);
 
   d3.select("svg")
     .attr("width", width)
-    .attr("height", height * 0.8);
+    .attr("height", height * 0.5);
+  legend.attr("transform", "translate(20,"+(height / 4 + 50)+")");
 
   d3.selectAll("path")
     .attr("d", path);
-
   slyelement.obj.reload();
 }
 
